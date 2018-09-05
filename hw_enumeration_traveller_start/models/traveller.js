cfac3a941024d2269d744fde5ed68477e7855af4
Fofiko <foteinid@gmail>
2018-09-05 21:46:36 +0100

    Semicolons

--- hw_enumeration_traveller_start/models/traveller.js
+++ hw_enumeration_traveller_start/models/traveller.js
@@ -3,34 +3,34 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-  const JourneyStartLocations = this.journeys.map ( journey => journey.startLocation )
+  const JourneyStartLocations = this.journeys.map ( journey => journey.startLocation );
   return JourneyStartLocations
 };
 
 
 Traveller.prototype.getJourneyEndLocations = function () {
-  const JourneyEndLocations = this.journeys.map ( journey => journey.endLocation )
-  return JourneyEndLocations
+  const JourneyEndLocations = this.journeys.map ( journey => journey.endLocation );
+  return JourneyEndLocations;
 };
 
 Traveller.prototype.getModesOfTransport = function () {
   const ModesOfTransport = this.journeys.map ( journey => journey.transport )
-  return ModesOfTransport
+  return ModesOfTransport;
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-  const JourneysOfSameTransport = this.journeys.filter ( journey => journey.transport === transport)
-  return JourneysOfSameTransport
+  const JourneysOfSameTransport = this.journeys.filter ( journey => journey.transport === transport);
+  return JourneysOfSameTransport;
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-  const JourneysOverMinDistance = this.journeys.filter ( journey => journey.distance >= minDistance)
-  return JourneysOverMinDistance
+  const JourneysOverMinDistance = this.journeys.filter ( journey => journey.distance >= minDistance);
+  return JourneysOverMinDistance;
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
   const TotalDistance = this.journeys.reduce ( (total, journey) => total += journey.distance, 0);
-  return TotalDistance
+  return TotalDistance;
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
